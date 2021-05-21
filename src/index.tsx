5c1c5a0fe5eb3bd8e85cd6d3991b9b55c11747bd
kanotan <kenta.kawano@toranoana.co.jp>
2021-05-21 11:56:51 +0000

    見た目を整える

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Amplify, { I18n } from 'aws-amplify';
-import { localization } from './assets/i18n/amplify/localization';
-import config from './aws-exports';
-Amplify.configure(config);
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import {AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange} from "@aws-amplify/ui-components";
+import NavBar from './layout/NavBar';
+import DemoContent from './layout/DemoContent';
 
-I18n.putVocabularies(localization);
-I18n.setLanguage('ja');
+const App = () => {
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<any>(); 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <div>
+        <NavBar username={user.username}/>
+        <DemoContent />
+    </div>
+  ) : (
+      <AmplifyAuthenticator />
+  );
+}
+
+export default App;
