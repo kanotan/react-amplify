b7dcd5539a98b745c6140335cf2a3c6ec05646d6
kanotan <kenta.kawano@toranoana.co.jp>
2021-05-21 11:50:57 +0000

    認証画面の翻訳設定を追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Amplify from 'aws-amplify';
+import Amplify, { I18n } from 'aws-amplify';
+import { localization } from './assets/i18n/amplify/localization';
 import config from './aws-exports';
 Amplify.configure(config);
+
+I18n.putVocabularies(localization);
+I18n.setLanguage('ja');
 
 ReactDOM.render(
   <React.StrictMode>
